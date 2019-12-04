470d61f2b0e700569946daa7b41896dc70e7467d
pizzafast <407649799@qq.com>
2019-12-04 16:47:05 +0800

    delete

--- TestGit/src/test/Git1.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public class Git1 {
-	public static void main(String[] args) {
-		System.out.println("fjlasfjldskafjladj");
-	}
-}
--- TestGit/src/test/Git2.java
+++
@@ -1,5 +0,0 @@
-package test;
-
-public class Git2 {
-	
-}
